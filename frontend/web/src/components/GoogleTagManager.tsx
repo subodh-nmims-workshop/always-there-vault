8e7a298f742cd26e1069bac92a3ca91a281a2eac
subodh-001 <subodh.ecoghar@gmail.com>
2026-07-13 22:34:17 +0530

    fix: update CSP to allow IPFS and Pinata fetches and propagate nonce in GoogleTagManager

--- frontend/web/src/components/GoogleTagManager.tsx
+++ frontend/web/src/components/GoogleTagManager.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+/* eslint-disable @next/next/next-script-for-ga */
 import Script from 'next/script';
 
 const GTM_ID = 'GTM-TW3VZ2X6';
@@ -12,16 +12,17 @@ export function GoogleTagManager({ nonce }: GoogleTagManagerProps) {
   return (
     <>
       {/* GTM Script in head */}
-      <Script
+      <script
         id="gtm-script"
-        strategy="afterInteractive"
         nonce={nonce}
         dangerouslySetInnerHTML={{
           __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            if ('${nonce}') { j.setAttribute('nonce', '${nonce}'); }
+            f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
           `
         }}
